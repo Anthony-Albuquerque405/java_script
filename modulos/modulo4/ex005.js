a3f032391df8149cbf2fc0193079ec17c6956454
Anthony-Albuquerque405 <anthonyhitnailbr@gmail.com>
2025-09-21 10:01:51 -0300

    Add examples for nested and multiple conditions in JS
    Added ex006.js and ex007.js demonstrating nested and multiple conditional statements. Updated ex005.js to refine voting logic with nested conditions. Included a VSCode launch configuration for debugging ex005.js.

--- modulos/modulo4/ex005.js
+++ modulos/modulo4/ex005.js
@@ -1,9 +1,12 @@
 // condições aninhadas
 
-var idade = 19;
+var idade = 65;
+console.log(`Você tem ${idade} anos.`);
 
-if (idade < 18) {
-  console.log("Menor de idade");
+if (idade < 16) {
+  console.log("Não vota");
+} else if (idade < 18 || idade >= 65) {
+  console.log("Voto opcional");
 } else {
-  console.log("Maior de idade");
+  console.log("Voto obrigatório");
 }
