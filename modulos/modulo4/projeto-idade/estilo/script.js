3483662304e1374edb1c8ce480d6df8c571c1814
Anthony-Albuquerque405 <anthonyhitnailbr@gmail.com>
2025-09-24 11:25:08 -0300

    Refactor image paths and update age verification logic
    Moved age-related images from 'imagens' to 'estilo' directory and updated script.js to use the new image paths. Refactored the age verification function for improved logic and fixed the function name in ex009.html. Also added CSS to hide the result image by default.

--- modulos/modulo4/projeto-idade/estilo/script.js
+++ modulos/modulo4/projeto-idade/estilo/script.js
@@ -1,4 +1,4 @@
-function verificar() {
+function verifivacar() {
   var data = new Date();
   var ano = data.getFullYear();
   var fano = document.getElementById("txtano");
@@ -8,59 +8,46 @@ function verificar() {
   } else {
     var fsex = document.getElementsByName("radsex");
     var idade = ano - Number(fano.value);
-    var gênero = "";
+    var genero = "";
     var img = document.createElement("img");
     img.setAttribute("id", "foto");
-
     if (fsex[0].checked) {
-      gênero = "Homem";
-      if (idade >= 0 && idade < 10) {
-        //Criança
-        img.setAttribute("src", "../imagens/homem-crianca.jpg");
-        img.style.width = "250px";
-        img.style.height = "250px";
-        img.style.marginTop = "20px";
-        img.style.borderRadius = "50%";
-        img.style.display = "block";
-        img.style.marginLeft = "auto";
-        img.style.marginRight = "auto";
-        img.style.objectFit = "cover";
-        img.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.5)";
-        img.style.border = "2px solid #000";
-        img.style.padding = "5px";
-        img.style.backgroundColor = "#f0f0f0";
+      genero = "Homem";
+      if (idade >= 0 && idade < 14) {
+        //criança
+        img.setAttribute("src", "homem-crianca.jpg");
+        var verifc = "criança";
+      } else if (idade < 21) {
+        //jovem
+        img.setAttribute("src", "homem-jovem.jpg");
+        var verifc = "jovem";
+      } else if (idade < 50) {
+        //adulto
+        img.setAttribute("src", "homem-adulto.jpg");
+        var verifc = "adulto";
+      } else {
+        //idoso
+        img.setAttribute("src", "homem-velho.jpg");
+        var verifc = "idoso";
       }
-    } else if (idade < 21) {
-      //Jovem
-      img.setAttribute("src", "../imagens/homem-jovem.jpg");
-    } else if (idade < 50) {
-      //Adulto
-      img.setAttribute(
-        "src",
-        "modulos/modulo4/projeto-idade/imagens/homem-crianca.jpg"
-      );
-    } else {
-      //Idoso
-      img.setAttribute("src", "../imagens/homem-velho.jpg");
+    } else if (fsex[1].checked) {
+      genero = "mulher";
+      if (idade >= 0 && idade < 14) {
+        //criança
+        img.setAttribute("src", "mulher-crianca.jpg");
+      } else if (idade < 21) {
+        //jovem
+        img.setAttribute("src", "mulher-jovem.jpg");
+      } else if (idade < 50) {
+        //adulto
+        img.setAttribute("src", "mulher-adulta.jpg");
+      } else {
+        //idoso
+        img.setAttribute("src", "mulher-velha.jpg");
+      }
     }
+    res.style.textAlign = "center";
+    res.innerHTML = `Detectamos uma ${genero} de ${idade} anos.`;
+    res.appendChild(img);
   }
-  if (fsex[1].checked) {
-    gênero = "Mulher";
-    if (idade >= 0 && idade < 10) {
-      //Criança
-      img.setAttribute("src", "../imagens/mulher-crianca.jpg");
-    } else if (idade < 21) {
-      //Jovem
-      img.setAttribute("src", "../imagens/mulher-jovem.jpg");
-    } else if (idade < 50) {
-      //Adulta
-      img.setAttribute("src", "../imagens/mulher-adulta.jpg");
-    } else {
-      //Idosa
-      img.setAttribute("src", "../imagens/mulher-velha.jpg");
-    }
-  }
-  res.style.textAlign = "center";
-  res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`;
-  res.appendChild(img);
 }
