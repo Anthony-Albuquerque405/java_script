5294755c07dedbb8d8874a4f72fad38771c06566
Anthony-Albuquerque405 <anthonyhitnailbr@gmail.com>
2025-09-25 20:41:42 -0300

    Add initial tabuada practice page with styles and script
    Created the HTML structure for a multiplication table (tabuada) practice page, including a styled layout and a basic JavaScript function to handle user input. Added CSS for visual styling and linked the script to the page.

---
+++ modulos/modulo5/pratica-tabuada/estilo/script.js
@@ -0,0 +1,10 @@
+function gerar() {
+  let num = document.getElementById("txtn");
+  let tab = document.getElementById("seltab");
+
+  if (num.value.length == 0) {
+    let n = Number(num.value);
+  } else {
+    alert("[ERRO] Por favor, digite um número!");
+  }
+}
